0c0c59ab58f68f066123634d59f4e8c2e5408ad0
Tyler Higgins <tyler.higgins@wsu.edu>
2020-03-25 08:57:15 -0700

    updating homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,46 +1,39 @@
 <template>
   <div class="home">
-    <b-carousel
-    id="carousel-1"
-    v-model="slide"
-    :interval="4000"
-    controls
-    img-width="1000"
-    img-height="400"
-    background="#ababab"
-    style="text-shadow: 1px 1px 2px #333;"
-    @sliding-start="onSlideStart"
-    @sliding-end="onSlideEnd"
-    >
-      <b-carousel-slide :img-src="require('@/assets/home/slide1.png')" caption="Toxic Angel Wolfy Creations!"
-      text="Freelance artist and crafter.">
+     <b-carousel
+             id="carousel-1"
+             v-model="slide"
+             :interval="4000"
+             controls
+             img-width="1280"
+             img-height="720"
+             background="#ababab"
+             style="text-shadow: 1px 1px 2px #333;"
+             @sliding-start="onSlideStart"
+             @sliding-end="onSlideEnd"
+     >
+       <b-carousel-slide :img-src="require('@/assets/home/slide1.png')" caption="Toxic Angel Wolfy Creations!"
+                         text="Freelance artist and crafter.">
         <span class="pr-sm-2">
         <b-button href="https://www.furaffinity.net/user/toxicangelwolfy">Furaffinity</b-button>
         </span>
-        <span class="pr-sm-2">
+         <span class="pr-sm-2">
         <b-button href="https://www.deviantart.com/toxicangelwolfy">DeviantArt</b-button>
         </span>
-        <span class="pr-sm-2">
+         <span class="pr-sm-2">
           <b-button href="https://twitter.com/wolfy_creations">Twitter</b-button>
         </span>
-        <span>
+         <span>
           <b-button href="https://www.instagram.com/toxicangelwolfy/">Instagram</b-button>
         </span>
-        <br><br>
-        <a class="btn" href="https://ko-fi.com/toxicangelwolfycreations">
-          Buy me a coffee!
-          <font-awesome-icon size="lg" icon="coffee"/>
-        </a>
-      </b-carousel-slide>
-      <b-carousel-slide :img-src="require('@/assets/home/slide2_adopt.png')"
-                        caption="Mystery Egg Adopts!" text="Price reduced!">
-        <b-button href="https://www.furaffinity.net/view/34595548/">Details</b-button>
-      </b-carousel-slide>
-      <b-carousel-slide :img-src="require('@/assets/home/adopt_slide_2.jpg')"
-      caption="Open Adopt!">
-        <b-button href="https://www.furaffinity.net/view/34891135/">Details</b-button>
-      </b-carousel-slide>
-    </b-carousel>
+         <br><br>
+         <a class="btn" href="https://ko-fi.com/toxicangelwolfycreations">
+           Buy me a coffee!
+           <font-awesome-icon size="lg" icon="coffee"/>
+         </a>
+       </b-carousel-slide>
+
+     </b-carousel>
   </div>
 </template>
 
@@ -56,5 +49,8 @@ export default {
 <style scoped lang="scss">
   #footer {
     padding-bottom: 100px;
+  }
+  #carousel-1 {
+    margin-top: 50px;
   }
 </style>
