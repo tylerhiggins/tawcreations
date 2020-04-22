e43d42f25adbec834f5106f63c46534789a5d49d
Tyler Higgins <tyler.higgins@wsu.edu>
2020-04-22 15:48:34 -0700

    added adopts to site

--- src/views/Adopts.vue
+++ src/views/Adopts.vue
@@ -1,20 +1,9 @@
 <template>
     <div class="adopts">
-        <b-alert
-                :show="dismissCountDown"
-                fade
-                variant="info"
-                @dismissed="dismissCountDown=0"
-                @dismiss-count-down="countDownChanged"
-        >
-            <p>Please read my <a class="alert-link" href="tos">Terms of Service</a> before buying an adopt.</p>
-        </b-alert>
         <h1>Current Adopts!</h1>
         <b-table-simple class="adopt-table" borderless>
             <colgroup><col><col></colgroup>
-            <b-thead>
-
-            </b-thead>
+            <b-thead></b-thead>
             <b-tbody>
                 <b-tr>
                     <b-th>
@@ -23,8 +12,8 @@
                 <p>Description: Fixed price for $20 for this Eastern Dragon!
                     <br>You can get a feral ref for an extra $10, chibi commission for an extra $5, and/or an icon for an extra $10!
                     <br>Base made by: <a href="https://www.furaffinity.net/user/ignitetheblaize"><font-awesome-icon icon="paw"/>&ensp;IgniteTheBlaize</a>
-                    </p>  
-                    <b-button href="https://www.furaffinity.net/view/35843385/">Claim this Eastern Dragon OC here!</b-button>
+                    </p>
+                    <b-button href="https://www.furaffinity.net/view/35843385/">Claim this Eastern Dragon!</b-button>
                     </b-th>
                     <b-th>
                         <h4 class="adopt-desc">Mystery Egg Adopts! (Only 4 Left!): $5</h4>
@@ -32,6 +21,27 @@
                 <p>Choose an egg and you will receive a mystery character reference sheet based on the egg color!<br>
                 Price is $5 per egg, or egg with head shot commission for $15!</p>
                 <b-button href="https://www.furaffinity.net/view/34595548/">Claim Here!</b-button>
+                    </b-th>
+                </b-tr>
+                <b-tr>
+                    <b-th>
+                        <h4 class="adopt-desc">Female Cat/Shark Hybrid: $50 or Trade</h4>
+                        <b-img class="adopt-img" :src="require('@/assets/adopts/wingedadopt.jpg')" fluid></b-img>
+                        <p>Description: This girl comes with this ref as well as a side, front and lineless head shot
+                            art made by me! I am willing to trade for another character with 6 or 7 pieces of art!
+                        <br>You can get an icon for an extra $10
+                        <br>Base made by: <a href="https://www.furaffinity.net/user/chesta"><font-awesome-icon icon="paw"/> Chesta</a>
+                        </p>
+                        <b-button href="https://www.furaffinity.net/view/36036146/">Claim Her Here!</b-button>
+                    </b-th>
+                    <b-th>
+                        <h4 class="adopt-desc">Male Bull Terrier Adopt: $20</h4>
+                        <b-img class="adopt-img" :src="require('@/assets/adopts/bulladopt.jpg')" fluid></b-img>
+                        <p>Description: This good boy is up for adoption for $20!<br>
+                        You can get a custom icon made by me for an extra $10!<br>
+                        Base made by: <a href="https://www.furaffinity.net/user/chesta"><font-awesome-icon icon="paw"/> Chesta</a>
+                        </p>
+                        <b-button href="https://www.furaffinity.net/view/36038556/">Claim This Good Boy!</b-button>
                     </b-th>
                 </b-tr>
             </b-tbody>
@@ -42,23 +52,7 @@
 <script>
     export default {
         name: "adopts",
-        data() {
-            return {
-                dismissSecs: 10,
-                dismissCountdown: 0,
-            }
-        },
-        created: function(){
-            this.showAlert()
-        },
-        methods: {
-            countDownChanged(dismissCountDown) {
-                this.dismissCountDown = dismissCountDown
-            },
-            showAlert() {
-                this.dismissCountDown = this.dismissSecs
-            },
-        }
+
     }
 </script>
 
@@ -72,6 +66,7 @@
         color: white;
     }
     .adopt-img {
+        max-width: 65%;
     }
     .adopt-desc {
         font-weight: bolder;
