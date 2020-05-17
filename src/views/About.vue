f7eef531da38d548c8b80348278ee5b1bf9cfde2
Tyler Higgins <tyler.higgins@wsu.edu>
2020-05-16 21:07:08 -0700

    fixed tos and about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,27 +1,34 @@
 <template>
   <div class="about">
-    <div>
-      <b-img id="avatar" :src="require('@/assets/about/avatar.jpg')"></b-img>
-    </div>
-    <div>
-      <p>
-        Hello! My name is Toxic Angel Wolfy! I am a freelance artist and crafter
-        specializing in artwork and crafts for the
-        <a href="https://furscience.com/whats-a-furry/"> furry fandom!</a> I
-        currently live in Oregon, U.S. and am engaged to a wonderful husky who
-        is named Lucky! My hobbies and interests include exploring new places,
-        drawing, and video gaming!<br /><br />My goal in life is to create a
-        better world through art and helping others. I would like to thank you
-        for your interest in my work, and hope that you enjoy my arts and
-        crafts!<br /><br />
-        Feel free to contact me through any of my social media pages, or email
-        me at
-        <a href="mailto: toxicangelwolfycreations@gmail.com"
-          >toxicangelwolfycreations@gmail.com</a
-        >
-        for any inquiries about my work!
-      </p>
-    </div>
+    <h1 class="text-center" id="abtitle">About Me!</h1>
+    <b-container>
+      <b-row>
+        <b-col>
+          <b-img id="avatar" :src="require('@/assets/about/avatar.jpg')" fluid></b-img>
+        </b-col>
+      </b-row>
+      <b-row class="mt-2">
+        <b-col>
+          <p>
+            Hello! My name is Toxic Angel Wolfy! I am a freelance artist and crafter
+            specializing in artwork and crafts for the
+            <a href="https://furscience.com/whats-a-furry/"> furry fandom!</a> I
+            currently live in Oregon, U.S. and am engaged to a wonderful husky who
+            is named Lucky! My hobbies and interests include exploring new places,
+            drawing, and video gaming!<br /><br />My goal in life is to create a
+            better world through art and helping others. I would like to thank you
+            for your interest in my work, and hope that you enjoy my arts and
+            crafts!<br /><br />
+            Feel free to contact me through any of my social media pages, or email
+            me at
+            <a href="mailto: toxicangelwolfycreations@gmail.com"
+            >toxicangelwolfycreations@gmail.com</a
+            >
+            for any inquiries about my work!
+          </p>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -36,13 +43,11 @@ export default {
   text-align: center;
 }
 p {
-  margin-left: 70px;
-  margin-right: 70px;
   color: white;
   font-weight: bold;
 }
-#avatar {
-  max-width: 75%;
-  color: white;
+#abtitle {
+  text-decoration: underline;
+  margin-bottom: 30px;
 }
 </style>
--- src/views/TOS.vue
+++ src/views/TOS.vue
@@ -1,118 +1,52 @@
 <template>
   <div class="tos">
-    <b-container>
-      <h1 class="pt-lg-3" id="tos_main_title">Terms of Service</h1>
-      <ul id="main-list">
-        <li class="tos-title">Artwork Terms of Service:</li>
-        <ul class="secondary-list">
-          <br />
-          <li>
-            My workdays are Monday-Friday. I do not work on Saturday or Sunday.
-          </li>
-          <br />
-          <li>
-            Commissions are sold as-is when completed. <span>NO REFUNDS</span>!
-          </li>
-          <br />
-          <li>
+    <h1 id="tos_main_title" class="text-center">Terms of Service</h1>
+    <b-container class="text-center">
+      <b-row>
+        <b-col>
+        <h3 class="tos-title">Artwork Terms of Service:</h3>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <p>
+            My workdays are Monday-Friday. I do not work on weekends (Saturday/Sunday).<br><br>
+            Commissions are sold as-is when completed. <span>NO REFUNDS</span>!<br><br>
             If you have already given me final approval on completed artwork and
-            wish to make a change, I will do so for a fee. I
-            <span>WILL NOT</span> make changes for free after the work is
-            complete.
-          </li>
-          <br />
-          <li>
-            I <span>WILL NOT</span> draw anything that involves scat, vomit, or
-            any sexual depiction of a minor.
-          </li>
-          <br />
-          <li>
-            I also <span>REFUSE</span> to draw anything that discriminates
-            against someones race, sexual orientation, and/or gender identity.
-          </li>
-          <br />
-          <li>
-            For commissions that involve gore: If there is someone else's
-            character(s) involved I need their written permission first!
-          </li>
-          <br />
-          <li>
-            You are required to be 18 or older (or of legal age in your
-            state/country), and must provide me your legal photo ID to
-            commission me for any sexually explicit and/or NSFW art!
-          </li>
-          <br />
-          <li>
-            For artwork that is shipped: I will provide a tracking number (if
-            shipping in the U.S.) and provide free shipping for any
-            at-convention artwork that requires it. For online commissions, you
-            <span>MUST</span> pay full shipping costs. If art gets lost in the
-            mail, you can get your art re-shipped for half of the commission
-            cost.
-          </li>
-          <br />
-          <li>
-            Any commission $100+ USD will be given a free badge of one of their
-            characters!
-          </li>
-          <br />
-          <li>
-            The only acceptable forms of payment for online commissions are
-            PayPal and DeviantArt Points!
-          </li>
-          <br />
-          <li>
-            At-convention acceptable forms of payment is PayPal, Debit/Credit
-            card, cash in the form of USD, and DeviantArt Points!
-          </li>
-          <br />
-          <li>
-            Please do not spam or harass me with messages about your artwork! If
-            you do, I will refund you and refuse any future
-            commissions/requests/trades from you!
-          </li>
-          <br />
-          <li>
-            Digital artwork may take up to two weeks to complete, and
-            Traditional artwork up to one week.
-          </li>
-          <br />
-          <li>
-            I will not start on your commission until payment is made in full.
-          </li>
-          <br />
-          <li>
-            Full refunds will only be made if I have not started your
-            commission.
-          </li>
-          <br />
-          <li>
-            If you are commissioning over $100+ USD worth of art from me, it may
-            take a month to fully complete all of your artwork.
-          </li>
-          <br />
-          <li>
-            You <span>MUST</span> credit me as the artist for any art you post
-            on any website or social media page!
-          </li>
-          <br />
-          <li>
-            I will not accept any partial payments for any artwork
-            <span>NO EXCEPTIONS</span>! If you cannot afford to pay, consider
-            doing an art-trade or making a request when I am open for those.
-          </li>
-          <br />
-          <li>
-            Paid commissions takes priority over art-trades and requests, so
-            they may take longer to complete.
-          </li>
-          <br />
-          <li>
-            I reserve the right to reject any commission/trade/request that I do
-            not feel comfortable with doing.
-          </li>
-        </ul>
-      </ul>
+            wish to make a change, I will do so for a fee. I <span>WILL NOT</span> make changes
+            for free after the work is complete.<br><br>
+            I <span>WILL NOT</span> draw anything that involves scat, vomit, or any sexual
+            depiction of a minor.<br><br>
+            I also <span>REFUSE</span> to draw anything that discriminates against someones race,
+            sexual orientation, and/or gender identity.<br><br>
+            For commissions that involve gore: If there is someone else's character(s) involved I
+            need their written permission first!<br><br>
+            You are required to be 18 or older (or of legal age in your state/country), and must
+            provide me your legal photo ID to commission me for any sexually explicit and/or NSFW art!<br><br>
+            For artwork that is shipped: I will provide a tracking number (if shipping in the U.S.)
+            and provide free shipping for any at-convention artwork that requires it.
+            For online commissions, you <span>MUST</span> pay full shipping costs. If art gets lost in
+            the mail, you can get your art re-shipped for half of the commission cost.<br><br>
+            Any commission $100+ USD will be given a free badge of one of their characters!<br><br>
+            The only acceptable forms of payment for online commissions are PayPal and DeviantArt Points!<br><br>
+            At-convention acceptable forms of payment is PayPal, Debit/Credit card, cash in the form of
+            USD, and DeviantArt Points!<br><br>
+            Please do not spam or harass me with messages about your artwork! If you do, I will refund
+            you and refuse any future commissions/requests/trades from you!<br><br>
+            Digital artwork may take up to two weeks to complete, and Traditional artwork up to one week.<br><br>
+            I will not start on your commission until payment is made in full.<br><br>
+            Full refunds will only be made if I have not started your commission.<br><br>
+            If you are commissioning over $100+ USD worth of art from me, it may take a month to fully
+            complete all of your artwork.<br><br>
+            You <span>MUST</span> credit me as the artist for any art you post on any website or social
+            media page!<br><br>
+            I will not accept any partial payments for any artwork <span>NO EXCEPTIONS</span>! If you cannot
+            afford to pay, consider doing an art-trade or making a request when I am open for those.<br><br>
+            Paid commissions takes priority over art-trades and requests, so they may take longer to complete.<br><br>
+            I reserve the right to reject any commission/trade/request that I do not feel comfortable with doing.
+          </p>
+        </b-col>
+      </b-row>
     </b-container>
   </div>
 </template>
@@ -124,13 +58,6 @@ export default {
 </script>
 
 <style scoped lang="scss">
-#tos_main_title {
-  margin-left: 25px;
-}
-.tos {
-  text-align: center;
-  margin-right: 50px;
-}
 .tos-title {
   list-style-type: none;
   text-decoration: underline;
@@ -143,7 +70,5 @@ export default {
 span {
   color: red;
 }
-ul {
-  list-style-type: none;
-}
+
 </style>
