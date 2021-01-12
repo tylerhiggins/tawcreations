5ab2c059f7186d4b1fbc5e9550b881f31202c157
Tyler Higgins <tyler.higgins@wsu.edu>
2021-01-11 23:13:58 -0800

    updated adopts

--- src/views/Adopts.vue
+++ src/views/Adopts.vue
@@ -8,7 +8,7 @@
           <b-img
             class="adopt-img"
             :src="require('@/assets/adopts/custom_ex.jpg')"
-            style="max-width: 40%"
+            style="max-width: 75%"
             fluid
             ></b-img>
           <p>
@@ -22,13 +22,34 @@
           >More Info</b-button
           >
         </b-col>
+        <b-col>
+          <h4 class="adopt-desc">Lava Cheetah: $45</h4>
+          <b-img
+              class="adopt-img"
+              :src="require('@/assets/adopts/lava_cheetah.jpg')"
+              style="max-width: 75%"
+              fluid
+          ></b-img>
+          <p>
+            Adopt this firey-quick Cheetah for only $45!<br />
+            Additional Options: <br />
+            Icon: +$10, General Reference Sheet: +$50, Adult Reference Sheet(ID Required): +$40 <br />
+            Base by <a href="https://www.furaffinity.net/user/chesta"
+          ><font-awesome-icon icon="paw" /> Chesta</a
+          >
+          </p>
+          <b-button href="https://www.furaffinity.net/view/39248112/"
+          >Claim Here! (FA Notes)</b-button
+          >
+        </b-col>
       </b-row>
-      <b-row>
+      <b-row class="mt-md-4">
         <b-col>
           <h4 class="adopt-desc">Mystery Egg Adopts! (Only 1 Left!): $5</h4>
           <b-img
             class="adopt-img"
             :src="require('@/assets/adopts/eggadopts.png')"
+            style="max-width: 40%"
             fluid
           ></b-img>
           <p>
@@ -38,47 +59,6 @@
           </p>
           <b-button href="https://www.furaffinity.net/view/34595548/"
             >Claim Here! (FA Notes)</b-button
-          >
-        </b-col>
-        <b-col>
-          <h4 class="adopt-desc">Mouse Adopt: $5</h4>
-          <b-img
-            class="adopt-img"
-            :src="require('@/assets/adopts/mouse.png')"
-            fluid
-          ></b-img>
-          <p>
-            I can add a custom ref sheet for an additional $50! (Feral +$25,
-            Adult Version(Id Required) +$40).<br />
-            This adopt is colored using a P2U base by:
-            <a href="https://www.furaffinity.net/user/gatorbite"
-              ><font-awesome-icon icon="paw" /> gatorbite</a
-            >
-          </p>
-          <b-button href="https://www.furaffinity.net/view/39930460/"
-            >Claim Here! (FA Notes)</b-button
-          >
-        </b-col>
-      </b-row>
-      <b-row class="mt-lg-4">
-        <b-col>
-          <h4 class="adopt-desc">Lava Cheetah: $45</h4>
-          <b-img
-              class="adopt-img"
-              style="max-width: 40%"
-              :src="require('@/assets/adopts/lava_cheetah.jpg')"
-              fluid
-          ></b-img>
-          <p>
-            Adopt this firey-quick Cheetah for only $45!<br />
-            Additional Options: <br />
-            Icon: +$10, General Reference Sheet: +$50, Adult Reference Sheet(ID Required): +$40 <br />
-            Base by <a href="https://www.furaffinity.net/user/chesta"
-            ><font-awesome-icon icon="paw" /> Chesta</a
-            >
-          </p>
-          <b-button href="https://www.furaffinity.net/view/39248112/"
-          >Claim Here! (FA Notes)</b-button
           >
         </b-col>
       </b-row>
