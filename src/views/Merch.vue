ade2787e5c50840e0a9edcd3b9f52fdf7eaf97d8
Tyler Higgins <tyler.higgins@wsu.edu>
2021-04-10 16:42:21 -0700

    testing buttons on sandbox mode

--- src/views/Merch.vue
+++ src/views/Merch.vue
@@ -15,7 +15,7 @@
       </b-row>
       <b-row>
         <b-col align="center" class="mt-md-2">
-          <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+          <form target="paypal" action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
             <input type="hidden" name="cmd" value="_s-xclick">
             <input type="hidden" name="hosted_button_id" value="T28SKHYAXSKNL">
             <table>
@@ -30,6 +30,7 @@
             <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
             <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
           </form>
+
         </b-col>
       </b-row>
       <b-row style="margin-top: 20px">
@@ -45,7 +46,7 @@
       </b-row>
       <b-row>
         <b-col align="center" class="mt-md-2">
-          <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+          <form target="paypal" action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
             <input type="hidden" name="cmd" value="_s-xclick">
             <input type="hidden" name="hosted_button_id" value="RAS327LPL56UL">
             <table>
@@ -60,6 +61,7 @@
             <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
             <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
           </form>
+
         </b-col>
       </b-row>
     </b-container>
