2be02528de515e4ce584f60127dc01a1ffdca9bc
Tyler Higgins <tyler.higgins@wsu.edu>
2021-04-10 18:05:07 -0700

    added official shop

--- src/views/Merch.vue
+++ src/views/Merch.vue
@@ -15,6 +15,21 @@
       </b-row>
       <b-row>
         <b-col align="center" class="mt-md-2">
+          <form target="_self" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+            <input type="hidden" name="cmd" value="_s-xclick">
+            <input type="hidden" name="hosted_button_id" value="NF24GGBL9ZRAG">
+            <table>
+              <tr><td><input type="hidden" name="on0" value="Sticker Type">Sticker Type</td></tr><tr><td><select name="os0">
+              <option value="Individual Sticker">Individual Sticker $1.00 USD</option>
+              <option value="Sticker Sheet">Sticker Sheet $5.00 USD</option>
+              <option value="Car Decal Vinyl Ind.">Car Decal Vinyl Ind. $10.00 USD</option>
+              <option value="Car Decal Vinyl Set">Car Decal Vinyl Set $20.00 USD</option>
+            </select> </td></tr>
+            </table>
+            <input type="hidden" name="currency_code" value="USD">
+            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+            <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+          </form>
 
         </b-col>
       </b-row>
@@ -31,8 +46,21 @@
       </b-row>
       <b-row>
         <b-col align="center" class="mt-md-2">
-
-
+          <form target="_self" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+            <input type="hidden" name="cmd" value="_s-xclick">
+            <input type="hidden" name="hosted_button_id" value="MRXDC66N8QC6W">
+            <table>
+              <tr><td><input type="hidden" name="on0" value="Sticker Type">Sticker Type</td></tr><tr><td><select name="os0">
+              <option value="Individual Sticker">Individual Sticker $1.00 USD</option>
+              <option value="Sticker Sheet">Sticker Sheet $5.00 USD</option>
+              <option value="Car Decal Vinyl Ind.">Car Decal Vinyl Ind. $10.00 USD</option>
+              <option value="Car Decal Vinyl Set">Car Decal Vinyl Set $20.00 USD</option>
+            </select> </td></tr>
+            </table>
+            <input type="hidden" name="currency_code" value="USD">
+            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+            <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+          </form>
         </b-col>
       </b-row>
     </b-container>
