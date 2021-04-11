a06f0ebc9af3bec280211d54f947ff92fea82b4f
Tyler Higgins <tyler.higgins@wsu.edu>
2021-04-10 17:17:03 -0700

    removed test buttons

--- src/views/Merch.vue
+++ src/views/Merch.vue
@@ -15,21 +15,7 @@
       </b-row>
       <b-row>
         <b-col align="center" class="mt-md-2">
-          <form target="paypal" action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
-            <input type="hidden" name="cmd" value="_s-xclick">
-            <input type="hidden" name="hosted_button_id" value="BL9RYQJ8LYS8L">
-            <table>
-              <tr><td><input type="hidden" name="on0" value="Sticker Type">Sticker Type</td></tr><tr><td><select name="os0">
-              <option value="Individual Sticker">Individual Sticker $1.00 USD</option>
-              <option value="Sticker Sheet">Sticker Sheet $5.00 USD</option>
-              <option value="Car Decal Individual">Car Decal Individual $10.00 USD</option>
-              <option value="Car Decal Set">Car Decal Set $20.00 USD</option>
-            </select> </td></tr>
-            </table>
-            <input type="hidden" name="currency_code" value="USD">
-            <input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-            <img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
-          </form>
+
         </b-col>
       </b-row>
       <b-row style="margin-top: 20px">
