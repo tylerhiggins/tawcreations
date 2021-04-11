b78aa878f68fad08a4b9478fdfa1331f62e8dcfb
Tyler Higgins <tyler.higgins@wsu.edu>
2021-04-10 17:02:36 -0700

    deleted buttons

--- src/views/Merch.vue
+++ src/views/Merch.vue
@@ -32,21 +32,7 @@
       </b-row>
       <b-row>
         <b-col align="center" class="mt-md-2">
-          <form target="paypal" action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
-            <input type="hidden" name="cmd" value="_s-xclick">
-            <input type="hidden" name="hosted_button_id" value="RAS327LPL56UL">
-            <table>
-              <tr><td><input type="hidden" name="on0" value="Sticker Type">Sticker Type</td></tr><tr><td><select name="os0">
-              <option value="Individual Sticker">Individual Sticker $1.00 USD</option>
-              <option value="Sticker Sheet">Sticker Sheet $5.00 USD</option>
-              <option value="Vinyl Car Decal Ind.">Vinyl Car Decal Ind. $10.00 USD</option>
-              <option value="Vinyl Car Decal Set">Vinyl Car Decal Set $20.00 USD</option>
-            </select> </td></tr>
-            </table>
-            <input type="hidden" name="currency_code" value="USD">
-            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-            <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-          </form>
+
 
         </b-col>
       </b-row>
